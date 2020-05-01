850bda43a010eb021af6a5238bd77a503b968634
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-05-01 19:48:12 +0100

    Seed 2 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+puts 'Creating restaurants...'
+Restaurant.create!({
+  name: "Le Dindon en Laisse",
+  address: "18 Rue Beautreillis, 75004 Paris, France"
+})
+Restaurant.create!({
+  name: "Neuf et Voisins",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+})
+puts 'Finished!'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
